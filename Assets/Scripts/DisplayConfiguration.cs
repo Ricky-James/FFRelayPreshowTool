91bbf3b5a2908a3a20a3261711e9d149c2b71c97
Ricky James <rickyjamesirl@gmail.com>
2023-01-11 23:30:00 +0000

    Added preprocessors to enable play mode testing

--- Assets/Scripts/DisplayConfiguration.cs
+++ Assets/Scripts/DisplayConfiguration.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class DisplayConfiguration : MonoBehaviour
 {
-//#if !UNITY_EDITOR
-    // Start is called before the first frame update
+#if UNITY_EDITOR // Sets control panel to screen 1 for debugging in play mode
     void Start()
     {
-        //Display.displays[0].SetParams(1280,720,Screen.width /2, Screen.height / 2);
+        GetComponent<Canvas>().targetDisplay = 0;
+        Image panel = GetComponentInChildren<Image>();
+        panel.transform.localScale = new Vector3(0.3f, 0.3f, 1);
+    }
+    
+#else
+    void Start()
+    {
         Display.displays[0].SetRenderingResolution(2560, 1440);
+        Screen.fullScreen = false;
         
-        
-        //Screen.SetResolution(1280,720,false,60);
         try
         {
             Display.displays[2].Activate();
@@ -25,8 +30,7 @@ public class DisplayConfiguration : MonoBehaviour
             GetComponent<Canvas>().targetDisplay = 1;
         }
         
-        Screen.fullScreen = false;
     }
-//#endif    
+#endif
 }
 
